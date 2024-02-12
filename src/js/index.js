a40b1f46d0e3d04fe5bffef2cc3fe9729c979889
Susana Torres Trujillo <149514280+susanatorrest@users.noreply.github.com>
2024-02-12 09:00:30 +0000

    fetch

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,14 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import ListItems from "./component/todo.jsx"
+
+const listItems=[
+    {label: "Make the bed", done : false},
+    {label: "Wash my hands", done : false},
+    {label: "Eat", done : false},
+    {label: "Go to the gym", done : false},
+    {label: "Run", done : true}];
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<ListItems  listItems={listItems} />, document.querySelector("#app"));
