0ee41b71636e8cf71393dcb1f2a6c2661581f772
Susana Torres Trujillo <149514280+susanatorrest@users.noreply.github.com>
2024-03-13 14:04:47 +0000

    m

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,143 +1,153 @@
 
 import React, { useState, useEffect } from "react";
 
-function ListItems(props) {
-    const [textValue, setTextValue] = useState("");
-    const [todos, setlistItems] = useState(props.listItems);
-    const [hoveredIndex, setHoveredIndex] = useState(null);
+function ListItems() {
+	const [textValue, setTextValue] = useState("");
+	const [todos, setlistItems] = useState([]);
 
-    useEffect(() => {
-		const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/8.6.1' } };
+	const createUser = () => {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify([])
+		};
 
 		fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
+			.then(response => response.json())
 			.then(response => {
-				if (!response.ok) {
-					throw Error(response.statusText);
+				getAllTask()
+			})
+			.catch(err => console.error(err));
+	}
+
+	const getAllTask = () => {
+		fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`)
+			.then(response => {
+				if (response.ok) {
+					return response.json;
 				}
-				return response.json();
+				else {
+					if (response.status === 404) {
+						createUser();
+					} else {
+						console.error("error en la solicitud", response.status)
+					}
+				}
 			})
 			.then(data => {
 				setlistItems(data)
 			})
 			.catch(err => {
-				if (err.message.includes("NOT FOUND")) {
-					const options = {
-						method: 'POST',
-						headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.1' },
-						body: JSON.stringify(todos)
-					};
-
-					fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
-						.then(response => response.json())
-						.then(response => console.log(response))
-						.catch(err => console.error(err));
-				}
-
 				console.error(err)
 			});
+	}
+
+	const deleteTask = (id) => {
+		const updateList = todos.filter((task) => task.id !== id)
+		setlistItems(updateList)
+		if (updateList.length === 0) {
+			const defaultTask = {
+				id: 1,
+				label: "default task",
+				done: false
+			}
+			updateList.push(defaultTask)
+		}
+		const options = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(updateList)
+		}
+
+		fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
+			.then(response => response.json())
+			.then(response => {
+				getAllTask()
+			})
+			.catch(err => console.error(err));
+	}
+
+const addTask = (value) => {
+	const newTask = {
+		label:value,
+		done:false,
+	}
+	const updateTask = [...todos, newTask]
+	const options = {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(updateTask)
+	}
+
+	fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
+		.then(response => response.json())
+		.then(response => {
+			setTextValue("")
+			getAllTask()
+		})
+		.catch(err => console.error(err));
+}
+
+
+	useEffect(() => {
+		getAllTask();
 	}, [])
 
-    function itemCloseButtonHandler(index) {
-        let nextTodos = [...todos];
-        nextTodos.splice(index, 1);
-        setlistItems(nextTodos);
-        setHoveredIndex(null);
-        const options = {
-										method: 'PUT',
-										headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.1' },
-										body: JSON.stringify(nextTodos)
-									};
 
-									fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
-										.then(response => response.json())
-										.then(data => {
-											const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/8.6.1' } };
-
-											fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
-												.then(response => response.json())
-												.then(retrievedTasks => {
-													setNewTask("")
-													setHoveredIndex(retrievedTasks)
-												})
-												.catch(err => console.error(err));
-
-										})
-										.catch(err => console.error(err));
+	return (
+		<>
+			<h1>todos</h1>
+			<div className="todo-list">
+				<input
+					type="text"
+					placeholder="What needs to be done?"
+					onChange={e => setTextValue(e.target.value)}
+					onKeyUp={(e) =>{
+						if (e.key === "Enter"){
+							addTask(textValue)
+						}
+					}}
+					value={textValue}
+				></input>
+				{
+					todos.map((item) =>{
+						return(
+							<li key={item.id}>
+								{
+									item.label
 								}
-
-    function onKeyUpHandler(e) {
-        if (e.key === "Enter" && e.target.value !== "") {
-            let nextTodos = [...todos, { label: e.target.value, done: false }];
-            setlistItems(nextTodos);
-            setTextValue("");
-        }
-        const options = {
-										method: 'PUT',
-										headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.6.1' },
-										body: JSON.stringify(nextTodos)
-									};
-
-									fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
-										.then(response => response.json())
-										.then(data => {
-											const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/8.6.1' } };
-
-											fetch(`https://playground.4geeks.com/apis/fake/todos/user/susanatorrest`, options)
-												.then(response => response.json())
-												.then(retrievedTasks => {
-													setTextValue("")
-													setlistItems(retrievedTasks)
-												})
-												.catch(err => console.error(err));
-
-										})
-										.catch(err => console.error(err));
-								}
-
-    let listItemsWithIndex = todos.map((e, i) => {
-        e.index = i;
-        return e;
-    });
-
-    let todoItems = listItemsWithIndex.filter(e => !e.done);
-    let itemsLeft = todos.reduce((a, c) => (a += c.done ? 0 : 1), 0);
-
-    return (
-        <>
-            <h1>todos</h1>
-            <div className="todo-list">
-                <input
-                    type="text"
-                    placeholder="What needs to be done?"
-                    onChange={e => setTextValue(e.target.value)}
-                    onKeyUp={onKeyUpHandler}
-                    value={textValue}
-                ></input>
-                {todoItems.length > 0 ? (
-                    <ul className="todo-items">
-                        {todoItems.map((listItem) => (
-                            <li
-                                key={listItem.index}
-                                onMouseEnter={() => setHoveredIndex(listItem.index)}
-                                onMouseLeave={() => setHoveredIndex(null)}
-                            >
-                                <span>{listItem.label}</span>
-                                {hoveredIndex === listItem.index && (
-                                    <button
-                                        onClick={() => itemCloseButtonHandler(listItem.index)}
-                                        className="fa fa-trash"
-                                    ></button>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No hay tareas, añadir tareas</p>
-                )}
-                <span>{itemsLeft} items left.</span>
-            </div>
-        </>
-    );
+								<i className="far fa-times-circle" onClick={() => {
+									deleteTask(item.id)
+								}}></i>
+							</li>
+						)
+					})
+				}
+				{/* {todoItems.length > 0 ? (
+					<ul className="todo-items">
+						{todoItems.map((listItem) => (
+							<li
+								key={listItem.index}
+								onMouseEnter={() => setHoveredIndex(listItem.index)}
+								onMouseLeave={() => setHoveredIndex(null)}
+							>
+								<span>{listItem.label}</span>
+								{hoveredIndex === listItem.index && (
+									<button
+										onClick={() => itemCloseButtonHandler(listItem.index)}
+										className="fa fa-trash"
+									></button>
+								)}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No hay tareas, añadir tareas</p>
+				)} */}
+				{/* <span>{itemsLeft} items left.</span> */}
+			</div>
+		</>
+	);
 }
 
 export default ListItems;
--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,6 @@ import "../styles/index.css";
 //import your own components
 import ListItems from "./component/todo.jsx"
 
-const listItems=[
-    {label: "Make the bed", done : false},
-    {label: "Wash my hands", done : false},
-    {label: "Eat", done : false},
-    {label: "Go to the gym", done : false},
-    {label: "Run", done : true}];
 
 //render your react application
-ReactDOM.render(<ListItems  listItems={listItems} />, document.querySelector("#app"));
+ReactDOM.render(<ListItems />, document.querySelector("#app"));
